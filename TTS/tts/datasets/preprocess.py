6899b6720c7a903b11701f8da92d3ccc1f4070f2
Katsuya Iida <katsuya.iida@gmail.com>
2021-05-15 14:24:42 +0900

    update

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -338,3 +338,18 @@ def _voxcel_x(root_path, meta_file, voxcel_idx):
 
     with open(str(cache_to), 'r') as f:
         return [x.strip().split('|') for x in f.readlines()]
+
+
+def kokoro(root_path, meta_file):
+    """Japanese single-speaker dataset from https://github.com/kaiidams/Kokoro-Speech-Dataset"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "kokoro"
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split('|')
+            wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
+            text = cols[2]
+            print(text)
+            items.append([text, wav_file, speaker_name])
+    return items
